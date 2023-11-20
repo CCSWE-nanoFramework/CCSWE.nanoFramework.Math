9ba8e185da17ab904b5c175c3cb3a82608c17b38
Cory Charlton <ccharlton@pobox.com>
2023-11-19 19:06:39 -0800

    Updating dependencies

--- CCSWE.nanoFramework.Math/FastMath.Clamp.cs
+++ CCSWE.nanoFramework.Math/FastMath.Clamp.cs
@@ -8,7 +8,7 @@ namespace CCSWE.nanoFramework
         //[MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static double Clamp(double value, double min, double max)
         {
-            if (min > max)
+            if (max < min)
             {
                 throw new ArgumentException();
             }
@@ -29,6 +29,25 @@ namespace CCSWE.nanoFramework
         //[MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float Clamp(float value, float min, float max)
         {
+            if (max < min)
+            {
+                throw new ArgumentException();
+            }
+
+            if (value < min)
+            {
+                return min;
+            }
+            else if (value > max)
+            {
+                return max;
+            }
+
+            return value;
+        }
+
+        public static int Clamp(int value, int min, int max)
+        {
             if (min > max)
             {
                 throw new ArgumentException();
@@ -48,10 +67,10 @@ namespace CCSWE.nanoFramework
 
         /// <inheritdoc cref="Math.Clamp(long,long,long)"/>
         //[MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static float Clamp(long value, long min, long max) => System.Math.Clamp(value, min, max);
+        internal static long Clamp(long value, long min, long max) => System.Math.Clamp(value, min, max);
 
         /// <inheritdoc cref="Math.Clamp(ulong,ulong,ulong)"/>
         //[MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static float Clamp(ulong value, ulong min, ulong max) => System.Math.Clamp(value, min, max);
+        internal static ulong Clamp(ulong value, ulong min, ulong max) => System.Math.Clamp(value, min, max);
     }
 }
